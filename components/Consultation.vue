8b3ffe20e880e00fc6fd4d06741c3b765885e16e
Yu Muramatsu <murayu@assistant-app.com>
2021-05-23 09:36:08 +0900

    サイズ・テキスト変更

--- components/Consultation.vue
+++ components/Consultation.vue
@@ -1,17 +1,11 @@
 <template>
   <v-col cols="12" md="6">
     <lazy-static-info
-      class="Consultation"
+      class="mb-4"
       url="https://www.city.sagamihara.kanagawa.jp/shisei/koho/1019896.html"
-      :text="$t('自分や家族の症状に不安や心配があれば\nまずは電話相談をどうぞ')"
+      :text="$t('自分や家族の症状に不安や心配があればまずは電話相談をどうぞ')"
       :btn-text="$t('相談の手順を見る')"
     />
   </v-col>
 </template>
 
-<style lang="scss" scoped>
-.Consultation {
-  padding: 3px 18px;
-  min-height: 5.5em;
-}
-</style>
