3f63f53084f31a4a301cf7615641fcbfdf8fb899
Yu Muramatsu <murayu@assistant-app.com>
2021-05-29 17:15:52 +0900

    改行に変更

--- components/SideNavigation.vue
+++ components/SideNavigation.vue
@@ -114,6 +114,7 @@
         <small class="SideNavigation-Copyright">
           &copy; 2020 Yu Muramatsu
         </small>
+        <br />
         <span id="ss_img_wrapper_2021gmogs_100-50_ja_t">
           <a href="https://jp.globalsign.com/" target="_blank" rel="nofollow"
             ><img
@@ -472,7 +473,6 @@ export default Vue.extend({
   line-height: 1.3;
   font-weight: 600;
   @include font-size(10);
-  margin-bottom: 1px;
 }
 
 .SideNavigation-LicenseLink {
