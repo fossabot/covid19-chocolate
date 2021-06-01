6aae6e9bcee7e35252e7faa2fa6d29fe16877fe9
Yu Muramatsu <murayu@assistant-app.com>
2021-06-01 16:07:17 +0900

    読み込み方法などを抜本的に改修 / DetailA

--- components/index/SiteTopUpper/DetailA.vue
+++ components/index/SiteTopUpper/DetailA.vue
@@ -1,69 +1,95 @@
 <template>
-  <div class="InfectionMedicalcareprovisionStatus">
-    <div class="InfectionMedicalcareprovisionStatus-heading">
-      <h3 class="InfectionMedicalcareprovisionStatus-title">
+  <div class="InfectionMedicalCareProvisionStatus">
+    <div class="InfectionMedicalCareProvisionStatus-heading">
+      <h3 class="InfectionMedicalCareProvisionStatus-title">
         {{ $t('本日の感染状況') }}
-        {{ date }}時点
+        {{ formatDate(date) }}時点
       </h3>
     </div>
-    <div class="InfectionMedicalcareprovisionStatus-Box">
-      <div class="InfectionMedicalcareprovisionStatus-Headline">
+    <div class="InfectionMedicalCareProvisionStatus-Box">
+      <div class="InfectionMedicalCareProvisionStatus-Headline">
         <app-link
           to="https://www.city.sagamihara.kanagawa.jp/shisei/koho/1019191.html"
         >
           {{ $t('感染状況') }}
         </app-link>
       </div>
-      <div class="InfectionMedicalcareprovisionStatus-description">
+      <div class="InfectionMedicalCareProvisionStatus-description">
         {{ $t('新規陽性者')
-        }}<span>{{ statuses.data['新規陽性者'].toLocaleString() }}人</span>（
+        }}<span>{{ statuses['新規陽性者'].toLocaleString() }}人</span>（
         {{ $t('濃厚接触者・接触者')
         }}<span
-          >{{ statuses.data['濃厚接触者・接触者'].toLocaleString() }}人</span
+          >{{ statuses['濃厚接触者・接触者'].toLocaleString() }}人</span
         >、{{ $t('海外渡航歴あり')
-        }}<span>{{ statuses.data['海外渡航歴あり'].toLocaleString() }}人</span
+        }}<span>{{ statuses['海外渡航歴あり'].toLocaleString() }}人</span
         >、{{ $t('不明')
-        }}<span>{{ statuses.data['不明'].toLocaleString() }}人</span>、
+        }}<span>{{ statuses['不明'].toLocaleString() }}人</span>、
         {{ $t('調査中')
-        }}<span>{{ statuses.data['調査中'].toLocaleString() }}人</span>）、
+        }}<span>{{ statuses['調査中'].toLocaleString() }}人</span>）、
         {{ $t('死亡者数')
-        }}<span>{{ statuses.data['死亡者数'].toLocaleString() }}人</span>
+        }}<span>{{ statuses['死亡者数'].toLocaleString() }}人</span>
       </div>
     </div>
-    <div class="InfectionMedicalcareprovisionStatus-Box">
-      <div class="InfectionMedicalcareprovisionStatus-Headline">
+    <div class="InfectionMedicalCareProvisionStatus-Box">
+      <div class="InfectionMedicalCareProvisionStatus-Headline">
         <app-link
           to="https://www.city.sagamihara.kanagawa.jp/shisei/koho/1019191.html"
         >
           {{ $t('症状') }}
         </app-link>
       </div>
-      <div class="InfectionMedicalcareprovisionStatus-description">
+      <div class="InfectionMedicalCareProvisionStatus-description">
         {{ $t('無症状')
-        }}<span>{{ statuses.data['無症状'].toLocaleString() }}人</span>、
+        }}<span>{{ statuses['無症状'].toLocaleString() }}人</span>、
         {{ $t('軽症')
-        }}<span>{{ statuses.data['軽症'].toLocaleString() }}人</span>、
+        }}<span>{{ statuses['軽症'].toLocaleString() }}人</span>、
         {{ $t('中等症')
-        }}<span>{{ statuses.data['中症'].toLocaleString() }}人</span>、
+        }}<span>{{ statuses['中症'].toLocaleString() }}人</span>、
         {{ $t('重症')
-        }}<span>{{ statuses.data['重症'].toLocaleString() }}人</span>
+        }}<span>{{ statuses['重症'].toLocaleString() }}人</span>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import dayjs from 'dayjs'
 import Vue from 'vue'
 
-import Data from '@/data/data.json'
+import AppLink from '@/components/_shared/AppLink.vue'
+import {
+  Data as IInfectionMedicalCareProvisionStatusData,
+  InfectionMedicalcareprovisionStatus as IInfectionMedicalCareProvisionStatus,
+} from '@/libraries/auto_generated/data_converter/convertInfectionMedicalcareprovisionStatus'
 
-export default Vue.extend({
-  data() {
-    return {
-      statuses: Data,
-      date: dayjs(Data.lastUpdate).format('YYYY年MM月DD日'),
-    }
+type Data = {}
+type Methods = {
+  formatDate(date: Date): string
+}
+type Computed = {
+  statuses: IInfectionMedicalCareProvisionStatusData
+  date: Date
+  statisticDate: Date
+  infectionMedicalCareProvisionStatus: IInfectionMedicalCareProvisionStatus
+}
+type Props = {}
+
+export default Vue.extend<Data, Methods, Computed, Props>({
+  components: {
+    AppLink,
+  },
+  computed: {
+    statuses() {
+      return this.infectionMedicalCareProvisionStatus.data
+    },
+    date() {
+      return new Date(this.infectionMedicalCareProvisionStatus.date)
+    },
+    statisticDate() {
+      return this.infectionMedicalCareProvisionStatus.data['検査統計日時']
+    },
+    infectionMedicalCareProvisionStatus() {
+      return this.$store.state.infectionMedicalCareProvisionStatus
+    },
   },
   methods: {
     formatDate(date) {
@@ -74,19 +100,19 @@ export default Vue.extend({
 </script>
 
 <style lang="scss">
-.InfectionMedicalcareprovisionStatus {
+.InfectionMedicalCareProvisionStatus {
   @include card-container();
 
   padding: 8px 18px;
   margin-bottom: 10px;
 
-  .InfectionMedicalcareprovisionStatus-heading {
+  .InfectionMedicalCareProvisionStatus-heading {
     display: flex;
     justify-content: space-between;
     align-items: center;
     flex-wrap: wrap;
 
-    .InfectionMedicalcareprovisionStatus-title {
+    .InfectionMedicalCareProvisionStatus-title {
       display: flex;
       align-items: center;
       padding: 0 0 4px 0;
@@ -97,10 +123,10 @@ export default Vue.extend({
     }
   }
 
-  .InfectionMedicalcareprovisionStatus-Box {
+  .InfectionMedicalCareProvisionStatus-Box {
     overflow: hidden;
 
-    .InfectionMedicalcareprovisionStatus-Headline {
+    .InfectionMedicalCareProvisionStatus-Headline {
       float: left;
       text-align: center;
       width: 10em;
@@ -112,7 +138,7 @@ export default Vue.extend({
       @include font-size(12);
     }
 
-    .InfectionMedicalcareprovisionStatus-description {
+    .InfectionMedicalCareProvisionStatus-description {
       @include font-size(12);
 
       padding: 3px 0 0 0;
