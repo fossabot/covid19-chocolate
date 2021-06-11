30748a049f9793b72fc5abaabecf555a71a621e6
Yu Muramatsu <murayu@assistant-app.com>
2021-06-11 09:42:06 +0900

    Update Card.vue

--- components/index/CardsMonitoring/ConfirmedCasesDetails/Card.vue
+++ components/index/CardsMonitoring/ConfirmedCasesDetails/Card.vue
@@ -14,20 +14,10 @@
             </li>
           </ul>
         </template>
-        <section>
-          <h4>{{ $t('概要') }}</h4>
-          <infection-status
-            :aria-label="$t('概要')"
-            :items="monitoringItems"
-          />
-        </section>
-        <section>
-          <h4>{{ $t('男女比') }}</h4>
-          <medical-system
-            :aria-label="$t('男女比')"
-            :items="monitoringItems"
-          />
-        </section>
+        <confirmed-cases-details-table
+          :aria-label="$t('検査陽性者の状況')"
+          v-bind="confirmedCases"
+        />
       </data-view>
     </client-only>
   </v-col>
@@ -39,14 +29,12 @@ import dayjs from 'dayjs'
 import DataView from '@/components/index/_shared/DataView.vue'
 // table タグとの衝突を避けるため ConfirmedCasesDetailsTable とする
 import ConfirmedCasesDetailsTable from '@/components/index/CardsMonitoring/ConfirmedCasesDetails/Table.vue'
-import ConfirmedCasesMWDetailsTable from '@/components/index/CardsMonitoring/ConfirmedCasesDetails/MWTable.vue'
 import Data from '@/data/data.json'
 import formatConfirmedCases from '@/utils/formatConfirmedCases'
 
 const options = {
   components: {
     DataView,
-    ConfirmedCasesMWDetailsTable,
     ConfirmedCasesDetailsTable,
   },
   data() {
@@ -67,50 +55,14 @@ export default options
 </script>
 
 <style lang="scss" module>
-section {
-  margin: 0 0 20px;
-
-  /* h タグが連続するため DataView-Content の margin を少し打ち消す */
-  &:first-child {
-    margin-top: -10px;
-  }
-
-  h4 {
-    margin: 5px 0 10px;
-    font-weight: normal;
-    @include font-size(16);
-  }
-}
-
 .button {
+  margin: 20px 0 0;
   color: $green-1 !important;
+  text-decoration: none;
   &:hover {
     color: $white !important;
   }
 
   @include button-text('sm');
-}
-
-dfn {
-  font-style: normal;
-  font-weight: 600;
-}
-
-details[open] .fade {
-  animation: fadeIn 0.7s ease;
-}
-@keyframes fadeIn {
-  0% {
-    opacity: 0;
-    transform: translateY(-10px);
-  }
-  100% {
-    opacity: 1;
-    transform: none;
-  }
-}
-
-details[open] summary {
-  color: gray;
 }
 </style>
