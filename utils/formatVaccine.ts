9500a7fccc6730ab42ba81f161ce17281e55b15d
Yu Muramatsu <murayu@assistant-app.com>
2021-06-19 13:35:14 +0900

    ワクチン接種状況に関するフォーマットファイルを作成

---
+++ utils/formatVaccine.ts
@@ -0,0 +1,71 @@
+type DataType = {
+  children: [
+    {
+      attr: '人口65歳以上'
+      value: number
+      children: [
+        {
+          attr: '接種済み'
+          value: number
+          children: [
+            {
+              attr: 'うち1回接種済み'
+              value: number
+            },
+            {
+              attr: 'うち2回接種済み'
+              value: number
+            }
+          ]
+        },
+        {
+          attr: '未接種'
+          value: number
+        }
+      ]
+    }
+  ]
+}
+
+type VaccineType = {
+  接種済み: number
+  うち1回接種済み: number
+  うち2回接種済み: number
+  未接種: number
+}
+
+function getSelectedItem(data: DataType, key: string) {
+  let result: number | undefined
+  const recursiveSearch = (data: ChildDataType) => {
+    if (result) {
+      return
+    }
+    if (data.attr === key) {
+      result = data.value
+    } else if (data.children) {
+      data.children.forEach((child: ChildDataType) => {
+        if (result) {
+          return
+        }
+        recursiveSearch(child)
+      })
+    }
+  }
+  recursiveSearch(data)
+
+  return result || 0
+}
+
+/**
+ * Format for *Chart component
+ *
+ * @param data - Raw data
+ */
+export default (data: DataType) => {
+  return {
+    接種済み: getSelectedItem(data, '接種済み'),
+    うち1回接種済み: getSelectedItem(data, 'うち1回接種済み'),
+    うち2回接種済み: getSelectedItem(data, 'うち2回接種済み'),
+    未接種: getSelectedItem(data, '未接種'),
+  } as VaccineType
+}
