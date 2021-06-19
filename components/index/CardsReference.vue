1e4ae0907e72b8b2df93567ec0ce6f54ca2cbe32
Yu Muramatsu <murayu@assistant-app.com>
2021-06-19 19:33:02 +0900

    カードの配置変更

--- components/index/CardsReference.vue
+++ components/index/CardsReference.vue
@@ -9,24 +9,6 @@ import Vue from 'vue'
 import CardsLazyRow from '@/components/index/_shared/CardsLazyRow.vue'
 
 // モニタリング項目
-const ConfirmedCasesByMunicipalitiesCard = () =>
-  import(
-    '@/components/index/CardsReference/ConfirmedCasesByMunicipalities/Card.vue'
-  )
-const DeathsByDeathDateCard = () =>
-  import('@/components/index/CardsReference/DeathsByDeathDate/Card.vue')
-const PositiveNumberByDiagnosedDateCard = () =>
-  import(
-    '@/components/index/CardsReference/PositiveNumberByDiagnosedDate/Card.vue'
-  )
-const PositiveNumberByDevelopedDateCard = () =>
-  import(
-    '@/components/index/CardsReference/PositiveNumberByDevelopedDate/Card.vue'
-  )
-const ConfirmedCasesByAgeCard = () =>
-  import('@/components/index/CardsReference/ConfirmedCasesByAge/Card.vue')
-const TestedNumberCard = () =>
-  import('@/components/index/CardsReference/TestedNumber/Card.vue')
 const VaccineInfo65Card = () =>
   import('@/components/index/CardsReference/VaccineInfo65/Card.vue')
 /* eslint-enable simple-import-sort/imports */
@@ -38,9 +20,6 @@ export default Vue.extend({
   data() {
     return {
       rows: [
-        [ConfirmedCasesByMunicipalitiesCard, DeathsByDeathDateCard],
-        [PositiveNumberByDiagnosedDateCard, PositiveNumberByDevelopedDateCard],
-        [ConfirmedCasesByAgeCard, TestedNumberCard],
         [VaccineInfo65Card],
       ],
     }
