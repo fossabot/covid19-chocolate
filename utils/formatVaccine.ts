f6b512d3ebd32aaf768a5a65cbd29c3b2ce924dc
Yu Muramatsu <murayu@assistant-app.com>
2021-07-03 15:56:28 +0900

    データの型を更新

--- utils/formatVaccine.ts
+++ utils/formatVaccine.ts
@@ -13,14 +13,18 @@ type DataType = {
               value: number
             },
             {
+              attr: 'うち1回接種割合'
+              value: number
+            },
+            {
               attr: 'うち2回接種済み'
+              value: number
+            },
+            {
+              attr: 'うち2回接種割合'
               value: number
             }
           ]
-        },
-        {
-          attr: '未接種'
-          value: number
         },
         {
           attr: 'うち1回接種'
@@ -39,8 +43,9 @@ type VaccineType = {
   人口65歳以上: number
   接種済み: number
   うち1回接種済み: number
+  うち1回接種割合: number
   うち2回接種済み: number
-  未接種: number
+  うち2回接種割合: number
   うち1回接種: number
   うち2回接種: number
 }
@@ -88,8 +93,9 @@ export default (data: DataType) => {
     人口65歳以上: getSelectedItem(data, '人口65歳以上'),
     接種済み: getSelectedItem(data, '接種済み'),
     うち1回接種済み: getSelectedItem(data, 'うち1回接種済み'),
+    うち1回接種割合: getSelectedItem(data, 'うち1回接種割合'),
     うち2回接種済み: getSelectedItem(data, 'うち2回接種済み'),
-    未接種: getSelectedItem(data, '未接種'),
+    うち2回接種割合: getSelectedItem(data, 'うち2回接種割合'),
     うち1回接種: getSelectedItem(data, 'うち1回接種'),
     うち2回接種: getSelectedItem(data, 'うち2回接種'),
   } as VaccineType
