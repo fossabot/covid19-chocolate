12204b60873ee7bf70cb2bb8a70a1f406e1f4760
Yu Muramatsu <murayu@assistant-app.com>
2021-07-04 15:23:31 +0900

    ALLOW MERGE

--- libraries/auto_generated/data_converter/convertData.ts
+++ libraries/auto_generated/data_converter/convertData.ts
@@ -7,7 +7,6 @@
 // These functions will throw an error if the JSON doesn't
 // match the expected interface, even if the JSON is valid.
 
-// テスト用コメント
 export interface Data {
     patientsSummary: PatientsSummary;
     lastUpdate:      string;
