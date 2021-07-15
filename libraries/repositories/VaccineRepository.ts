26c2aaed6aa7dc9f89aab9c79a02873f74b4abfc
Yu Muramatsu <murayu@assistant-app.com>
2021-07-15 16:40:10 +0900

    fix path

--- libraries/repositories/VaccineRepository.ts
+++ libraries/repositories/VaccineRepository.ts
@@ -1,26 +1,25 @@
 // eslint-disable-next-line import/no-named-default
-import { default as rawData } from '@/data/vaccine.json'
+import { default as rawData } from '@/data/vaccination.json'
 import {
   Convert,
-  Vaccine,
-} from '@/libraries/auto_generated/data_converter/convertVaccine'
+  Vaccination,
+} from '@/libraries/auto_generated/data_converter/convertVaccination'
 import {
   BaseRepository,
   IBaseRepository,
 } from '@/libraries/repositories/BaseRepository'
 
-export interface IVaccineRepository extends IBaseRepository<Vaccine> {}
-
-export class VaccineRepository
-  extends BaseRepository<Vaccine>
-  implements IVaccineRepository
+export interface IVaccinationRepository extends IBaseRepository<Vaccination> {}
+export class VaccinationRepository
+  extends BaseRepository<Vaccination>
+  implements IVaccinationRepository
 {
   /**
    * 使用箇所
    *
-   * 65歳以上のワクチン接種状況
+   * ワクチン接種回数（高齢者・累計） (components/index/CardsReference/Vaccination/Card.vue)
    */
   constructor() {
-    super(Convert.toVaccine(JSON.stringify(rawData)))
+    super(Convert.toVaccination(JSON.stringify(rawData)))
   }
 }
