0502eba9e76204435c9240b56340d4622bcdc450
Yu Muramatsu <murayu@assistant-app.com>
2021-07-15 19:02:28 +0900

    Import path

--- store/index.ts
+++ store/index.ts
@@ -3,6 +3,7 @@ import { DataRepository } from '@/libraries/repositories/DataRepository'
 // InfectionMedicalcareprovisionStatus ではなく InfectionMedicalCareProvisionStatus とする
 import { InfectionMedicalcareprovisionStatusRepository as InfectionMedicalCareProvisionStatusRepository } from '@/libraries/repositories/InfectionMedicalCareProvisionStatusRepository'
 import { NewsRepository } from '@/libraries/repositories/NewsRepository'
+import { VaccinationRepository } from '@/libraries/repositories/VaccinationRepository'
 
 export const state = () => ({
   dailyPositiveDetail: new DailyPositiveDetailRepository().data,
@@ -10,4 +11,5 @@ export const state = () => ({
   infectionMedicalCareProvisionStatus:
     new InfectionMedicalCareProvisionStatusRepository().data,
   news: new NewsRepository().data,
+  vaccination: new VaccinationRepository().data,
 })
