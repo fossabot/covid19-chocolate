19ac8cfe75d6dbb7238e0a480dbd90b2419367e6
Yu Muramatsu <murayu@assistant-app.com>
2021-07-18 20:47:46 +0900

    [WIP] 数値の入力ミスに関するお知らせを追加

---
+++ pages/data.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="About">
+    <page-header :title="$t('【開発中】数値の入力ミスに関するお知らせ(2021年7月19日)')" />
+    <static-card>
+      {{
+        $t(
+          '当サイトを閲覧していただきありがとうございます。'
+        )
+      }}<br />
+      <br />
+      {{
+        $t(
+          '今回、一部グラフにて表示されている数値において入力ミスがありましたのでご報告させていただきます。'
+        )
+      }}<br />
+      <br />
+      {{
+        $t(
+          '1. 入力ミスのあったグラフ及び数値'
+        )
+      }}
+        {{
+          $t(
+            '報告日別による陽性者数の推移：2021年7月1日において12名と相模原市から発表されていた数値を当サイトにて7名として表記'
+          )
+        }}
+        {{
+          $t(
+            '新規陽性者数'
+          )
+        }}
+    </static-card>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { MetaInfo } from 'vue-meta'
+
+import AppLink from '@/components/_shared/AppLink.vue'
+import PageHeader from '@/components/_shared/PageHeader.vue'
+import StaticCard from '@/components/_shared/StaticCard.vue'
+
+export default Vue.extend({
+  components: {
+    PageHeader,
+    StaticCard,
+    AppLink,
+  },
+  head(): MetaInfo {
+    return {
+      title: this.$t('数値の入力ミスに関するお知らせ') as string,
+    }
+  },
+  methods: {
+    formatDate(date) {
+      return `${this.$d(date, 'date')}`
+    },
+  },
+})
+</script>
