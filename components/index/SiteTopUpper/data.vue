b4f288fdef8846e5b66eb9aefee6c67030b4d8a2
Yu Muramatsu <murayu@assistant-app.com>
2021-07-19 17:50:08 +0900

    数値ミスをホームから報告できるようにする

---
+++ components/index/SiteTopUpper/data.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-col cols="12" md="6">
+    <v-lazy>
+      <static-info
+        class="Data"
+        url=""
+        :text="
+          $t('当サイトの数値に問題があれば\nご報告ください')
+        "
+        :btn-text="$t('数値ミスを報告する(外部サービスを使用しています)')"
+      />
+    </v-lazy>
+  </v-col>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import StaticInfo from '@/components/index/SiteTopUpper/Consultation/StaticInfo.vue'
+
+type Data = {}
+type Methods = {}
+type Computed = {}
+type Props = {}
+
+export default Vue.extend<Data, Methods, Computed, Props>({
+  components: {
+    StaticInfo,
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.Data {
+  padding: 3px 18px;
+  min-height: 5.5em;
+}
+</style>
