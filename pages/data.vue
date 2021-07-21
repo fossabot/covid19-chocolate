938d6a77f9dd59fb8fb58c8ecf8c597c52aafadb
Yu Muramatsu <murayu@assistant-app.com>
2021-07-21 14:36:54 +0900

    【公開用】URLを更新

--- pages/data.vue
+++ pages/data.vue
@@ -3,7 +3,7 @@
     <page-header :title="$t('数値の入力ミスに関するお知らせ')" />
     <static-card>
       <app-link
-        to="https://sagamihara-stopcovid19.com/info/0720"
+        to="https://sagamihara-stopcovid19.com/info/0721"
         :icon-size="12"
         >{{ $t('2021年7月21日発表分 (別タブで開きます)') }}
       </app-link>
