bfe27f0261736d6d49fe6cbfcb680b78bfeb98ec
Yu Muramatsu <murayu@assistant-app.com>
2021-07-27 14:45:34 +0900

    PATHをjsonから読み込むように変更

--- ui-test/ogp_screenshot.py
+++ ui-test/ogp_screenshot.py
@@ -1,26 +1,14 @@
 import os
 import time
+import json
 
 from selenium import webdriver
 
 if not os.path.exists("ogp"):
     os.mkdir("ogp")
 
-PATHS = {
-    "/?dummy": (959, 500),
-    "/cards/details-of-confirmed-cases": (959, 500),
-    "/cards/number-of-confirmed-cases": (959, 500),
-    "/cards/positive-number-by-diagnosed-date":(959, 730),
-    "/cards/monitoring-number-of-confirmed-cases": (959, 500),
-    "/cards/monitoring-items-overview": (959, 570),
-    "/cards/number-of-confirmed-cases-by-municipalities": (959, 570),
-    "/cards/positive-number-by-developed-date": (959, 570),
-    "/cards/deaths-by-death-date": (959, 570),
-    "/cards/number-of-confirmed-cases-by-age": (959, 570),
-    "/cards/tested-number": (959, 570),
-    "/cards/vaccine-info": (959, 570),
-    "/cards/vaccination": (959, 570),
-}
+f = open('../assets/json/cardRoutesSettings.json', 'r')
+card_data = json.load(f)
 
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")
@@ -31,8 +19,9 @@ driver = webdriver.Chrome(options=options)
 for lang in ("ja", "en", "zh-cn", "zh-tw", "ko"):
     if not os.path.exists("ogp/{}".format(lang)):
         os.mkdir("ogp/{}".format(lang))
-    for path, size in PATHS.items():
-        driver.set_window_size(*size)
+    for value in card_data:
+        driver.set_window_size(*(value['ogpWidth'], value['ogpHeight']))
+        path = value['path']
         driver.get(
             "http://localhost:8000{}?ogp=true".format(
                 path if lang == "ja" else "/{}{}".format(lang, path)
