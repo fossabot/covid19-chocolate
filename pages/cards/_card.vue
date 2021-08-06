802dcb87080af4b72b1874cae976017fc544f47e
Yu Muramatsu <murayu@assistant-app.com>
2021-08-06 20:03:52 +0900

    新規グラフを追加

--- pages/cards/_card.vue
+++ pages/cards/_card.vue
@@ -34,6 +34,8 @@ import ConfirmedCasesByAgeCard from '@/components/index/CardsReference/Confirmed
 import TestedNumberCard from '@/components/index/CardsReference/TestedNumber/Card.vue'
 // 新型コロナウイルスワクチン接種者数
 import VaccineInfoCard from '@/components/index/CardsReference/VaccineInfo/Card.vue'
+// 未定
+import Vaccine7Card from '@/components/index/CardsReference/Vaccine7/Card.vue'
 // 65歳以上のワクチン接種状況
 import VaccinationCard from '@/components/index/CardsReference/VaccineInfo65/Card.vue'
 
@@ -59,6 +61,7 @@ import type { NuxtConfig } from '@nuxt/types'
     ConfirmedCasesByAgeCard,
     TestedNumberCard,
     VaccineInfoCard,
+    Vaccine7Card,
     VaccinationCard,
   },
 })
@@ -122,6 +125,11 @@ export default class CardContainer extends Vue implements NuxtConfig {
       // 新型コロナウイルスワクチン接種者数
       case 'vaccine-info':
         cardComponent = 'vaccine-info-card'
+        cardCategory = 'reference'
+        break
+      // 未定
+      case 'vaccine-7':
+        cardComponent = 'vaccine-7-card'
         cardCategory = 'reference'
         break
       // 65歳以上のワクチン接種状況
